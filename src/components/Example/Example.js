8d239e39923a26e5f1930981e56f415f4682536c
Naomi-Pate <patenaomi99@gmail.com>
2023-10-25 11:55:31 -0400

    updated example.js since it's not recieving anything

--- src/components/Example/Example.js
+++ src/components/Example/Example.js
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Example() {
-    const API_URL = process.env.REACT_APP_API_URL;
-    const [exampleData, setExampleData] = useState([]);
+  const API_URL = process.env.REACT_APP_API_URL;
+  const [exampleData, setExampleData] = useState([]);
 
-    console.log(API_URL);
+  console.log(API_URL);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    //fetchData();
+  }, []);
 
-    async function fetchData() {
-        // We reach out to our .env file to get the API URL
-        // We use the "REACT_APP_" prefix to make sure that our variables are available in the frontend
-        const response = await axios.get(`${API_URL}/example`);
+  async function fetchData() {
+    // We reach out to our .env file to get the API URL
+    // We use the "REACT_APP_" prefix to make sure that our variables are available in the frontend
+    const response = await axios.get(`${API_URL}/example`);
 
-        // We set the data from the response to our "exampleData" state
-        setExampleData(response.data);
-    }
+    // We set the data from the response to our "exampleData" state
+    setExampleData(response.data);
+  }
 
-    return (
-        <div className="App">
-            {exampleData.map((item) => {
-                return (
-                    <div key={item.id}>
-                        <h2>{item.name}</h2>
-                        <p>{item.description}</p>
-                        <p>{item.price}</p>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="App">
+      {exampleData.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Example;
