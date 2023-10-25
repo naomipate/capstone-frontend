61b389de6946a3f26a421384899dac9bd6bd09bf
Naomi-Pate <patenaomi99@gmail.com>
2023-10-25 19:26:58 -0400

    fix errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // COMPONENTS
-import Example from "./components/Example/Example";
+//import Example from "./components/Example/Example";
 
 function App() {
   return (
