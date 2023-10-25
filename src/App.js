ccdc14c842334f44925e7c00d14650e73a78e6f7
Naomi-Pate <patenaomi99@gmail.com>
2023-10-25 11:58:36 -0400

    updated app.js to display hellow world for now

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Example />} />
+        <Route path="/" element={<h1>Hello World!</h1>} />
       </Routes>
     </Router>
   );
