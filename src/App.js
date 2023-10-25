f90b0729da4199c6e3de0686bab8aff3ac12aab0
Naomi-Pate <patenaomi99@gmail.com>
2023-10-25 11:38:06 -0400

    Updated App.js to route to Example component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // DEPENDENCIES
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // COMPONENTS
-import Example from './components/Example/Example';
+import Example from "./components/Example/Example";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Hello world!</h1>} />
+        <Route path="/" element={<Example />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
