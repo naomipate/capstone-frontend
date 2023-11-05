5f6b7371fe79fcf256b985cc0612ee4c48404031
Naomi-Pate <patenaomi99@gmail.com>
2023-11-04 23:42:07 -0400

    added backend deploy link to .env and brought into Axios.js

--- src/components/API/Axios.js
+++ src/components/API/Axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:8080"
-      : "DEPLOY URL",
-      timeout: 50000,
+      ? process.env.REACT_APP_API_URL
+      : process.env.REACT_APP_DEPLOY_API_URL,
+  timeout: 50000,
 });
 
 export default AxiosInstance;
