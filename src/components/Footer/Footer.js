bea1be3daca93f1b376f1714af5fb441448acbf2
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-11-09 11:38:32 -0500

    Cleaned up reorganized footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,19 +2,81 @@ import "./Footer.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import GiftuneLogo from "../../Assets/GiftuneLogo.png"
+
 function Footer() {
   return (
-      <div className="footer">
-        {/* © Giftune Inc 2023 */}
-        <div className="footer-links">
-          <NavLink to={"/"}>Home</NavLink>
-          <NavLink to={"/login"}>Login</NavLink>
-          <NavLink to={"/signup"}>Signup</NavLink>
-          <NavLink to={"/about"}>About</NavLink>
-          <NavLink>Find Friend</NavLink>
+    // <div className="footer">
+    //   <div className="content">
+    //     <h3 className="title">Giftune</h3>
+    //     <p className="text">
+    //       An app designed to keep you on top of your loved ones upcoming
+    //       birthdays, where you can effortlessly select the perfect gift from a
+    //       diverse array of options.
+    //     </p>
+    //     <p className="copyright">
+    //       copyright ©
+    //       <NavLink to={"#"} className={"copy-link"}>
+    //         Giftune Inc 2023
+    //       </NavLink>
+    //     </p>
+    //   </div>
+    //   <div className="bottom">
+    //     <div className="menu">
+    //       <ul className="list">
+    //         <li className="item">
+    //           <NavLink to={"/"} className={"link"}>
+    //             Home
+    //           </NavLink>
+    //         </li>
+    //         <li className="item">
+    //           <NavLink to={"/login"} className={"link"}>
+    //             Login
+    //           </NavLink>
+    //         </li>
+    //         <li className="item">
+    //           <NavLink to={"/signup"} className={"link"}>
+    //             SignUp
+    //           </NavLink>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="footer">
+      <div className="content">
+        <div>
+        <img className="footer-image" src={GiftuneLogo} alt="Logo"/>
+        <p className="slogan">"Where heartfelt gifting meets perfect harmony."</p>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <NavLink className="link" to={"/"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to={"/login"}>Login</NavLink>
+            </li>
+            <li className="link">
+              <NavLink className="link" to={"/signup"}>Signup</NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to={"/about"}>About Us</NavLink>
+            </li>
+          </ul>
         </div>
       </div>
+      <hr />
+      <div className="bottom">
+        <p className="text">An app designed to keep you on top of your loved ones upcoming
+        birthdays, where you can effortlessly select the perfect gift from a
+        diverse array of options.
+        </p>
+        <p className="copyright">copyright ©</p>
+      </div>
+    </div>
   );
+  // <div className="footer">© Giftune Inc 2023</div>;
 }
 
 export default Footer;
