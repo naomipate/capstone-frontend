ff30aa589d455b00bf66f691a599a4e2f12f087d
Chantal <chantalgomez@pursuit.org>
2023-11-09 12:19:42 -0500

    commit for merge

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import "./FriendList.css";
 import { getAllFriendsFromUser, getFriendsWishlist } from "../API/API";
 
 
 function FriendList() {
   const [friendsList, setFriendsList] = useState([]);
+  const { id } = useParams();
+
 
   useEffect(() => {
     fetchList();
-  }, []);
+  }, [id]);
 
   async function fetchList() {
     try {
@@ -25,9 +28,16 @@ function FriendList() {
 
   return (
     <div>
-        <ul>
-            {/* {friendsList.map(({}))} */}
-        </ul>
+      <h1> friends</h1>
+        {/* <ul>
+            {friendsList.map(({id, user_name}))} => {
+              return (
+                <li key={id}> 
+                {user_name}
+                </li>
+              )
+            }
+        </ul> */}
     </div>
   )
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,47 +4,47 @@ import { useParams, useNavigate } from "react-router-dom";
 import SidebarNav from "../SidebarNav/SidebarNav";
 
 function Sidebar() {
-  const [user, setUser] = useState({});
-  const navigate = useNavigate();
+//   const [user, setUser] = useState({});
+//   const navigate = useNavigate();
 
-  const { id } = useParams();
-  useEffect(() => {
-    fetchData(id);
-  }, []);
+//   const { id } = useParams();
+//   useEffect(() => {
+//     fetchData(id);
+//   }, []);
 
-  const fetchData = async () => {
-    try {
-      let response = await Axios.get(`/dashboard/${id}`);
-      console.log(response.data);
-      setUser(response.data);
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div>
-      <div>Sidebar</div>
-      {user.userProfile && <UserDetails userDetails={user?.userProfile} />}
-      <SidebarNav />
-    </div>
-  );
-}
+//   const fetchData = async () => {
+//     try {
+//       let response = await Axios.get(`/dashboard/${id}`);
+//       console.log(response.data);
+//       setUser(response.data);
+//       return response.data;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   return (
+//     <div>
+//       <div>Sidebar</div>
+//       {user.userProfile && <UserDetails userDetails={user?.userProfile} />}
+//       <SidebarNav />
+//     </div>
+//   );
+// }
 
-function UserDetails({ userDetails }) {
-  console.log(userDetails);
-  let { first_name, last_name, dob } = userDetails;
-  let date = new Date(dob);
-  return (
-    <div>
-      <div>
-        <h3>
-          {first_name} {last_name}
-        </h3>
-        <p>{date.toLocaleDateString()}</p>
-      </div>
-    </div>
-  );
+// function UserDetails({ userDetails }) {
+//   console.log(userDetails);
+//   let { first_name, last_name, dob } = userDetails;
+//   let date = new Date(dob);
+//   return (
+//     <div>
+//       <div>
+//         <h3>
+//           {first_name} {last_name}
+//         </h3>
+//         <p>{date.toLocaleDateString()}</p>
+//       </div>
+//     </div>
+//   );
 }
 
 export default Sidebar;
