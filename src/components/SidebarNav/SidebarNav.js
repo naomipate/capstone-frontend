5627808231481e42ba020f49c693f3cf945630db
Chantal <chantalgomez@pursuit.org>
2023-11-09 12:20:32 -0500

    commit for merge

--- src/components/SidebarNav/SidebarNav.js
+++ src/components/SidebarNav/SidebarNav.js
@@ -3,32 +3,32 @@ import Axios from "../API/Axios";
 import { useParams, useNavigate } from "react-router-dom";
 
 function SidebarNav() {
-  // const [user, setUser] = useState({});
-  // const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
 
-  // const { id } = useParams();
-  // useEffect(() => {
-  //   fetchData(id);
-  // }, []);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchData(id);
+  }, []);
 
-  // const fetchData = async () => {
-  //   try {
-  //     let response = await Axios.get(`/dashboard/${id}`);
-  //     console.log(response.data);
-  //     setUser(response.data);
-  //     return response.data;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  // return (
-  //   <div>
-  //     <div>Sidebar Nav</div>
-  //     <div>Number of friends and link to friends list</div>
-  //     <div>Notifications</div>
-  //     <div>Wishlist</div>
-  //   </div>
-  // );
+  const fetchData = async () => {
+    try {
+      let response = await Axios.get(`/dashboard/${id}`);
+      console.log(response.data);
+      setUser(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <div>Sidebar Nav</div>
+      <div>Number of friends and link to friends list</div>
+      <div>Notifications</div>
+      <div>Wishlist</div>
+    </div>
+  );
 }
 
 export default SidebarNav;
