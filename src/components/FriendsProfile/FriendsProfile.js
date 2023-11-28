9b13d100c9f7a60e3610f153cc2ad73ba3ef64c7
Chantal <chantalgomez@pursuit.org>
2023-11-28 11:03:00 -0500

    styling friend profile

--- src/components/FriendsProfile/FriendsProfile.js
+++ src/components/FriendsProfile/FriendsProfile.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import FriendsProfileWishlist from "./FriendsProfileWishlist/FriendsProfileWishlist";
 import { getFriendsAndTheirWishlists } from "../API/API";
+import { TbArrowLeft } from "react-icons/tb";
+import { IconContext } from "react-icons";
 import "./FriendsProfile.css";
 
 function FriendsProfile() {
@@ -9,6 +11,7 @@ function FriendsProfile() {
   const [friendInfoWishList, setFriendInfoWishList] = useState([]);
 
   const { id, friendId } = useParams();
+  let navigate = useNavigate();
 
   useEffect(() => {
     fetchList();
@@ -27,39 +30,48 @@ function FriendsProfile() {
   }
 
   return (
-    <div className="friend-profile-container">
-      <div className="friend-profile-info-top">
-        <div className="friend-wishlist-top-left-side">
-          <img
-            alt="friend-user-profile"
-            className="friend-user-profile"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUdhnWROHccYu5AG4Ahi_WnaQgxINV9abPz1MqdYXFwT4txCA5"
-          />
-          <div className="friend-user-names">
-            <h2>{friendInfoProfile.user_name}</h2>
-            <p>{friendInfoProfile.first_name} {friendInfoProfile.last_name}</p>
-            <p className="friend-user-dob">
-            {new Date(friendInfoProfile.dob)
-              .toDateString()
-              .split(" ")
-              .splice(1, 2)
-              .join(" ")}
-          </p>
+      <div className="friend-profile-container">
+        <div className="friend-profile-info-top">
+          <div className="friend-wishlist-top-left-side">
+            <img
+              alt="friend-user-profile"
+              className="friend-user-profile"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUdhnWROHccYu5AG4Ahi_WnaQgxINV9abPz1MqdYXFwT4txCA5"
+            />
+            <div className="friend-user-names">
+              <h2>{friendInfoProfile.user_name}</h2>
+              <p>
+                {friendInfoProfile.first_name} {friendInfoProfile.last_name}
+              </p>
+              <p className="friend-user-dob">
+                {new Date(friendInfoProfile.dob)
+                  .toDateString()
+                  .split(" ")
+                  .splice(1, 2)
+                  .join(" ")}
+              </p>
+            </div>
+          </div>
+
+          <div className="friend-wishlist-top-right-side">
+            <button className="button-friend-profile">Unfollow</button>
           </div>
         </div>
-
-        <div className="friend-wishlist-top-right-side">
-          <button className="button-friend-profile">Unfollow</button>
+        <div className="friend-wishlist-list-container">
+          <ul className="friend-wishlist-ul">
+            {friendInfoWishList.map((item) => {
+              return <FriendsProfileWishlist item={item} />;
+            })}
+          </ul>
         </div>
+        <IconContext.Provider
+        value={{ size: "2rem" }}
+      >
+        <div onClick={() => navigate(-1)} className="back-left-arrow-container">
+          <TbArrowLeft />
+        </div>
+      </IconContext.Provider>
       </div>
-      <div className="friend-wishlist-list-container">
-        <ul className="friend-wishlist-ul">
-          {friendInfoWishList.map((item) => {
-            return <FriendsProfileWishlist item={item} />;
-          })}
-        </ul>
-      </div>
-    </div>
   );
 }
 
