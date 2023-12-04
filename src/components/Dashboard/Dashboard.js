e764ae26d4b61d846a9fedbb22d496a2e2921464
Naomi-Pate <patenaomi99@gmail.com>
2023-12-04 09:00:39 -0500

    moved calculateZodiacSign cb into current year condition

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -46,58 +46,54 @@ function Dashboard() {
       let upcomingDateWithNextYear = new Date(
         date.setFullYear(currentDate.getFullYear() + 1)
       );
-      console.log(calculateZodiacSign(upcomingDateWithNextYear));
 
       return upcomingDateWithNextYear.getTime();
     }
   };
+
+  function calculateZodiacSign(dob) {
+    let zodiacLookUp = {
+      capricorn: { start: "12-22", end: "01-19" },
+      aquarius: { start: "01-20", end: "02-18" },
+      pisces: { start: "02-19", end: "03-20" },
+      aries: { start: "03-21", end: "04-19" },
+      taurus: { start: "04-20", end: "05-20" },
+      gemini: { start: "05-21", end: "06-20" },
+      cancer: { start: "06-21", end: "07-22" },
+      leo: { start: "07-23", end: "08-22" },
+      virgo: { start: "08-23", end: "09-22" },
+      libra: { start: "09-23", end: "10-22" },
+      scorpio: { start: "10-23", end: "11-21" },
+      sagittarius: { start: "11-22", end: "12-21" },
+    };
+
+    // Convert the dob to the current year for comparison
+    let dobDate = new Date(dob);
+    let currentDate = new Date();
+    dobDate.setFullYear(currentDate.getFullYear());
+    let dobInMili = dobDate.getTime();
+
+    for (let zodiac in zodiacLookUp) {
+      let { start, end } = zodiacLookUp[zodiac];
+
+      if (dobInMili >= start && dobInMili <= end) {
+        return zodiac;
+      }
+    }
+
+    return "Unknown zodiac sign";
+  }
+
   user?.friendsOrderedByDOB?.forEach((friend) => {
     friend.dobInMili = upcomingDateCalc(friend.dob);
+    console.log(calculateZodiacSign(friend.dobInMili));
   });
 
   let friendsList = user?.friendsOrderedByDOB?.map((friendDetails, index) => {
     return <Friend key={index} friendDetails={friendDetails} id={id} />;
   });
 
-  return (
-    <div className="dashboard-container">
-      {/* <div>Dashboard</div> */}
-      {friendsList}
-      {/* <Sidebar /> */}
-    </div>
-  );
-}
-
-function calculateZodiacSign(dob) {
-  let zodiacLookUp = {
-    capricorn: { start: "2022-12-22", end: "2023-01-19" },
-    aquarius: { start: "2023-01-20", end: "2023-02-18" },
-    pisces: { start: "2023-02-19", end: "2023-03-20" },
-    aries: { start: "2023-03-21", end: "2023-04-19" },
-    taurus: { start: "2023-04-20", end: "2023-05-20" },
-    gemini: { start: "2023-05-21", end: "2023-06-20" },
-    cancer: { start: "2023-06-21", end: "2023-07-22" },
-    leo: { start: "2023-07-23", end: "2023-08-22" },
-    virgo: { start: "2023-08-23", end: "2023-09-22" },
-    libra: { start: "2023-09-23", end: "2023-10-22" },
-    scorpio: { start: "2023-10-23", end: "2023-11-21" },
-    sagittarius: { start: "2023-11-22", end: "2023-12-21" },
-  };
-
-  let dobInMili = new Date(dob).getTime();
-  console.log(dobInMili);
-
-  for (let zodiac in zodiacLookUp) {
-    let start = new Date(zodiacLookUp[zodiac].start).getTime();
-    let end = new Date(zodiacLookUp[zodiac].end).getTime();
-
-    if (dobInMili >= start && dobInMili <= end) {
-      return zodiac;
-    }
-  }
-
-  // Handles the case where the date falls outside the provided ranges
-  return "Unknown zodiac sign";
+  return <div className="dashboard-container">{friendsList}</div>;
 }
 
 function Friend({ friendDetails, id }) {
