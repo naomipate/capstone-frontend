522032a9fd9e6bde9260ce8cf9b3e9975052c52e
Naomi Pate <68704431+naomipate@users.noreply.github.com>
2024-01-05 20:21:39 -0500

    Update Dashboard.js clearing out console logs

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -12,9 +12,7 @@ function Dashboard({ user }) {
   const [dashboardId, setDashboardId] = useState(user.id);
   const [dashboardUser, setDashboardUser] = useState({});
   let currentDate = new Date(Date.now()); // Time from system
-  console.log("Before: " + currentDate);
   currentDate.setTime(currentDate.getTime() + (currentDate.getTimezoneOffset() * 60 * 1000));
-  console.log("After: " + currentDate);
   const { setFriendsData } = useContext(FriendsContext);
 
   useEffect(() => {
@@ -55,14 +53,12 @@ function Dashboard({ user }) {
     // Sort by this ^^^^^
     if (upcomingDateDiff > 0) {
       // positive is in the current year
-      console.log(new Date(upcomingDateWithCurrentYear));
       return upcomingDateWithCurrentYear;
     } else {
       // negative is next year
       let upcomingDateWithNextYear = new Date(
         date.setFullYear(currentDate.getFullYear() + 1)
       );
-      console.log(upcomingDateWithNextYear);
       return upcomingDateWithNextYear;
     }
   };
